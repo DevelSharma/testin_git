8a943c3cdf5535199f06241c6afdbf959802c4f4
DevelSharma <develsharma93@gmail.com>
2024-01-25 19:13:11 -0500

    version 1

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1. Select all elements
+const form = document.querySelector('#new-item-form')
+const list = document.querySelector('#list')
+const input = document.querySelector('#item-input')
+
+
+// When I submit the form add a new element
+/* here an even is added to the form element which is
+protecting the default settings that is refreshing the page 
+**/
+form.addEventListener('submit', e=> {
+    e.preventDefault()
+
+    // 1. Create a new item
+    const item = document.createElement('div')
+    item.innerText = input.value
+    item.classList.add('list-item')
+
+    // 2. Add that item to the list
+    list.appendChild(item)
+
+    // 3. Clear input
+    input.value =""
+    // 4. Setup event listner to delete item when clicked
+    item.addEventListener('click', () =>{
+        list.removeChild(item)
+    })
+})
